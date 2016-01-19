9c7a55936d9a6efb24600d0b74ce70e54174e041
Christiaan Baaij <christiaan.baaij@gmail.com>
2016-01-19 17:27:46 +0100

    Remove workaround for GHC issue 11230

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -14,6 +14,7 @@ import Prelude hiding (head,tail,init,(++),splitAt,concat,drop)
 import qualified Prelude as P
 
 import Data.List (isInfixOf)
+import Data.Proxy
 import Control.Exception
 import Test.Tasty
 import Test.Tasty.HUnit
